4267a14ab0559f291c333b1c3cfdc76dd9a33e70
171250035 <954853048@qq.com>
2018-05-25 08:19:36 +0800

    倒计时
    .

--- ELApp/AppEL/src/main/java/com/example/lenovo/elapp/ClockActivity.java
+++ ELApp/AppEL/src/main/java/com/example/lenovo/elapp/ClockActivity.java
@@ -1,8 +1,16 @@
 package com.example.lenovo.elapp;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import com.moos.library.CircleProgressView;
+
+import Managers.CountingDown;
+import cn.iwgang.countdownview.CountdownView;
 
 /**
  * Created by hxh on 2018/5/24.
@@ -10,22 +18,32 @@ import android.view.View;
 
 public class ClockActivity extends AppCompatActivity {
     private Thread thread;
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clock);
+        setContentView(R.layout.circle);
 
-        CircleView circleView = (CircleView) findViewById(R.id.circle_view);
-        circleView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (thread != null) {
-                    thread.interrupt();
-                    thread = null;
-                }
-                thread = new Thread(circleView);
-                thread.start();
-            }
-        });
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        //CircleProgressView
+        CircleProgressView circleProgressView = findViewById(R.id.progressView_circle);
+        circleProgressView.setStartProgress(0);
+        circleProgressView.setEndProgress(100);
+        //circleProgressView.setTrackColor(R.color.white);
+        circleProgressView.setCircleBroken(true);
+        circleProgressView.setTrackWidth(20);
+        circleProgressView.setProgressDuration(5*1000);
+        circleProgressView.setTrackEnabled(true);
+        circleProgressView.setFillEnabled(false);
+        circleProgressView.startProgressAnimation();
+        circleProgressView.setProgressTextColor(Color.WHITE);
+
+        CountdownView countdownView=findViewById(R.id.countingDown);
+        CountingDown countingDown=CountingDown.getCountingDown(this);
+        countingDown.start(5,countdownView);
     }
 }
