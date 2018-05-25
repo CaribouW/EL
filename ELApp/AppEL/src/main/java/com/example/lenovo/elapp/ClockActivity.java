77e2ca09074aadd788c0fe80633e858a99419743
171250035 <954853048@qq.com>
2018-05-25 15:16:29 +0800

    解决冲突
    。

--- ELApp/AppEL/src/main/java/com/example/lenovo/elapp/ClockActivity.java
+++ ELApp/AppEL/src/main/java/com/example/lenovo/elapp/ClockActivity.java
@@ -7,19 +7,16 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-<<<<<<< HEAD
 import android.view.View;
 import android.widget.Button;
 
 import com.moos.library.CircleProgressView;
 
 import Activitys.MainActivity;
-=======
 
 import com.moos.library.CircleProgressView;
 
 import BackUps.WinJudgement;
->>>>>>> 86e3315c2c931159a259e2080aa3243aa9c81374
 import Managers.CountingDown;
 import Managers.MusicManager;
 import Managers.Task;
@@ -47,7 +44,6 @@ public class ClockActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
-<<<<<<< HEAD
         Button circleBrn = (Button) findViewById(R.id.circle_return);
         circleBrn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,8 +52,6 @@ public class ClockActivity extends AppCompatActivity {
             }
         });
 
-=======
->>>>>>> 86e3315c2c931159a259e2080aa3243aa9c81374
         //CircleProgressView
         CircleProgressView circleProgressView = findViewById(R.id.progressView_circle);
         CountdownView countdownView = findViewById(R.id.countingDown);
@@ -75,13 +69,8 @@ public class ClockActivity extends AppCompatActivity {
         circleProgressView.setEndProgress(100);
         //circleProgressView.setTrackColor(R.color.white);
         circleProgressView.setCircleBroken(true);
-<<<<<<< HEAD
         circleProgressView.setTrackWidth(30);
         circleProgressView.setProgressDuration(5*1000);
-=======
-        circleProgressView.setTrackWidth(20);
-        circleProgressView.setProgressDuration(second * 1000);
->>>>>>> 86e3315c2c931159a259e2080aa3243aa9c81374
         circleProgressView.setTrackEnabled(true);
         circleProgressView.setFillEnabled(false);
         circleProgressView.startProgressAnimation();
